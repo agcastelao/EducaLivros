8d14eb5efb735ee438451be106dfb260f79afb27
FelipeMoraes18 <111572342+FelipeMoraes18@users.noreply.github.com>
2024-05-16 12:32:12 -0300

    Atualizcao react (sem o css)

--- educa-livros-app/src/App.js
+++ educa-livros-app/src/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Biblioteca from './Components/Biblioteca/Biblioteca';
 import Ecommerce from './Components/Ecommerce/Ecommerce';
 import HomePage from './Components/HomePage/HomePage';
 import Informacoes from './Components/Informacoes/Informacoes';
-import Login from './Components/Login/Login'
-import Footer from './Components/Footer/Footer'
+import Login from './Components/Login/Login';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
-    <div>
-      <HomePage />
-      <Informacoes />
-      <Biblioteca />
-      <Ecommerce />
-      <Login />
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<HomePage />} exact />
+          <Route path="/informacoes" element={<Informacoes />} />
+          <Route path="/biblioteca" element={<Biblioteca />} />
+          <Route path="/ecommerce" element={<Ecommerce />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
