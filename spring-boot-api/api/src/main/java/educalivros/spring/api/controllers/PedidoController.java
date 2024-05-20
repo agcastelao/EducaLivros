d6eb1ad475afe4ce216c8c46891401fa79d31ffa
jcbarros24 <126578116+jcbarros24@users.noreply.github.com>
2024-05-20 15:06:03 -0300

    Commit pós teste com sucesso do migration pós tabela pedido

---
+++ spring-boot-api/api/src/main/java/educalivros/spring/api/controllers/PedidoController.java
@@ -0,0 +1,37 @@
+package educalivros.spring.api.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import educalivros.spring.api.ValueObjects.V1.PedidoVO;
+import educalivros.spring.api.services.PedidoService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/pedido")
+public class PedidoController {
+
+    @Autowired
+    PedidoService pedidoService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public PedidoVO createPedido(@RequestBody PedidoVO pedido) {
+        
+        return pedidoService.createPedido(pedido);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<PedidoVO> getPedido() {
+        
+        return pedidoService.findAllPedidos();
+    }
+    
+}
